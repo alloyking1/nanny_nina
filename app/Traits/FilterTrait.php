985214afd7fb10f16cb360647e2255e5a1149023
alloyking1 <alloyking1@gmail.com>
2022-05-18 14:22:34 +0100

    feat:moved filter functionality to FilterTrait.php

--- app/Traits/FilterTrait.php
+++ app/Traits/FilterTrait.php
@@ -2,7 +2,12 @@
 
 namespace App\Traits;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use Exception;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+use App\Http\Resources\UserResource;
+
 
 trait FilterTrait
 {
@@ -14,5 +19,29 @@ trait FilterTrait
 
     public function filterRequest()
     {
+        try {
+            //implement filtering
+            $users = QueryBuilder::for(User::class)
+                ->allowedFilters([
+                    'name',
+                    AllowedFilter::exact('age'),
+                    'sate', 'email',
+                    'country',
+                    AllowedFilter::exact('code'),
+                    AllowedFilter::callback('has_posts', fn (Builder $query) => $query->whereHas('posts')),
+
+                ])
+                ->get();
+
+            return response()->json([
+                'message' => new UserResource($users)
+            ], 200);
+        } catch (Exception $e) {
+
+            return response()->json([
+                "error" => "Internal server error",
+                "status" => 500
+            ]);
+        }
     }
 }
