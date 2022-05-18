fd8840ca32bb76216c6d901751412f29dee381ed
alloyking1 <alloyking1@gmail.com>
2022-05-18 13:58:10 +0100

    feat: moving filter feature in controller to filter trait

---
+++ app/Traits/FilterTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+
+trait FilterTrait
+{
+
+    /**
+     * @param Request $request Model
+     * @return Response
+     */
+
+    public function filterRequest()
+    {
+    }
+}
